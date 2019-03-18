238d82c2ded06ae0a64ea64fba0a3afedcc5365d
Zisis Palaskas <zpalaskas@gmail.com>
2019-03-18 15:17:47 +0200

    Fixed not importing db when running app

--- app.py
+++ app.py
@@ -14,9 +14,6 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'jose'
 api = Api(app)
 
-@app.before_first_request
-def create_tahles():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
